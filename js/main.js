2356f65bcda853ac653219b56e115ce5738f4230
Bram <b.van.oosterhout@mcw.nl>
2022-06-13 12:08:24 +0200

    Added dynamic screen toggle manager

--- js/main.js
+++ js/main.js
@@ -11,38 +11,22 @@ function checkDevMode() {
 
 function init() {
   checkDevMode();
-  document.getElementById('attract-screen').classList.add('active');
 }
 
 init();
 
-let elements = document.getElementsByClassName("cheat-button");
 
-let toggleScreen = function() {
-  let attribute = this.getAttribute("id");
-  // alert(attribute);
-  switch (attribute) {
-    case 'toggle-attract':
-      console.log('break 1');
-      document.getElementById('attract-screen').classList.add('active');
-      document.getElementById('intro-screen').classList.remove('active');
-      document.getElementById('game-screen').classList.remove('active');
-      break;
-    case 'toggle-intro':
-      console.log('break 2');
-      document.getElementById('attract-screen').classList.remove('active');
-      document.getElementById('intro-screen').classList.add('active');
-      document.getElementById('game-screen').classList.remove('active');
-      break;
-    case 'toggle-game':
-      console.log('break 3');
-      document.getElementById('attract-screen').classList.remove('active');
-      document.getElementById('intro-screen').classList.remove('active');
-      document.getElementById('game-screen').classList.add('active');
-      break;
-  }
-};
+// 
+// SCREEN TOGGLE MANAGER
+// 
+const cheatButtons = document.querySelectorAll(".cheat-button");
+const allScreens = document.querySelectorAll(".screen");
 
-Array.from(elements).forEach(function(element) {
-  element.addEventListener('click', toggleScreen);
-});
+for(let i = 0; i < cheatButtons.length; i++) {
+  cheatButtons[i].addEventListener("click", function(e) {
+    for(let x = 0; x < allScreens.length; x++) {
+      allScreens[x].classList.remove("active");
+    }
+    allScreens[i].classList.add("active");
+  });
+}
